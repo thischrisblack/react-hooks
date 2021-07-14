e5d4672508c234b381a2e95b83eba536c2e31882
Chris Black <chrisblack@Chriss-MBP.attlocal.net>
2021-07-13 23:21:00 -0400

    Ex 2, Extra Credit 2.

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -8,7 +8,7 @@ function Greeting({initialName = ''}) {
 
   React.useEffect(() => {
     window.localStorage.setItem('name', name)
-  })
+  }, [name])
 
   function handleChange(event) {
     setName(event.target.value)
